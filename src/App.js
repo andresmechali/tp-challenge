0cb447579aca978a10d33655bc66bc89a9cd52c6
andres <andresmechali@gmail.com>
2018-07-05 19:21:08 +0200

    remove router and add product name

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import data from './reviews.json';
 
 import Main from './containers/Main';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { reviews } = data;
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Route exact path="/" component={Main} />
-          </div>
-        </Router>
+        <Main reviews={reviews} productName="Fake product" />
       </div>
     );
   }
